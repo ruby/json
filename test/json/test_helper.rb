18150e0d78abfacfdff71f3ffa0e3aaf70d196ad
Robin Miller <robin@tenjin.ca>
2025-09-10 15:57:37 -0600

    Enable coverage for all files and add branch coverage tracking.

--- test/json/test_helper.rb
+++ test/json/test_helper.rb
@@ -1,14 +1,6 @@
 $LOAD_PATH.unshift(File.expand_path('../../../ext', __FILE__), File.expand_path('../../../lib', __FILE__))
 
-begin
-  require 'simplecov'
-rescue LoadError
-  # Don't fail Ruby's test suite
-else
-  SimpleCov.start
-end
-
-require 'json'
+require "coverage"
 require 'test/unit'
 
 if ENV["JSON_COMPACT"]
@@ -35,3 +27,32 @@ unless defined?(Test::Unit::CoreAssertions)
   require "core_assertions"
   Test::Unit::TestCase.include Test::Unit::CoreAssertions
 end
+
+Test::Unit.at_exit do
+  begin
+    require 'simplecov'
+  rescue LoadError
+    # Don't fail Ruby's test suite
+  else
+    # Force SimpleCov to include all files in its report, avoiding accidental require-order misses
+    SimpleCov.track_files 'lib/**/*.rb'
+
+    SimpleCov.add_filter 'lib/json/truffle_ruby' unless RUBY_ENGINE == 'truffleruby'
+
+    SimpleCov.enable_coverage :branch
+    SimpleCov.primary_coverage :branch
+
+    # must be true for SimpleCov to generate a result at all
+    SimpleCov.running = true
+    coverage = SimpleCov.result
+
+    SimpleCov.write_last_run(coverage)
+    coverage.format!
+  end
+end
+
+# Start built-in Coverage directly because SimpleCov depends on JSON gem, wrecking the require order.
+# SimpleCov is still used for the pretty formatting afterward.
+# require is at end of file to avoid coverage monitoring any irrelevant code (eg. Test::Unit)
+Coverage.start(lines: true, branches: true)
+require 'json'
